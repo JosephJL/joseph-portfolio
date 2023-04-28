7e763f972ce0e0e5e11dda80695b4170ba5e3679
JosephJL <HeeJLJoseph@gmail.com>
2023-04-28 10:49:02 +0800

    Update Todo.tsx
    added update and delete

--- src/component/Todo.tsx
+++ src/component/Todo.tsx
@@ -8,6 +8,20 @@ type TodoProps = {
 export default function Todo({ todo }: TodoProps) {
   const { id, text, done } = todo;
 
+  const trpc = api.useContext();
+
+  const { mutate: doneMutation } = api.todo.toggle.useMutation({
+    onSettled: async () => {
+      await trpc.todo.all.invalidate();
+    },
+  });
+
+  const { mutate: deleteMutation } = api.todo.delete.useMutation({
+    onSettled: async () => {
+      await trpc.todo.all.invalidate();
+    },
+  });
+
   return (
     <>
       <div className="justify between flex items-center gap-2">
@@ -18,12 +32,20 @@ export default function Todo({ todo }: TodoProps) {
             id="done"
             className="border-gray-30 h-4 w-4 cursor-pointer rounded border bg-gray-50"
             checked={done}
+            onChange={(e) => {
+              doneMutation({ id, done: e.target.checked });
+            }}
           />
           <label htmlFor="done" className={`cursor-pointer`}>
             {text}
           </label>
         </div>
-        <button className="bg-blue-700 text-white hover:bg-blue-800 focus:ring-4">
+        <button
+          className="bg-blue-700 text-white hover:bg-blue-800 focus:ring-4"
+          onClick={() => {
+            deleteMutation(id);
+          }}
+        >
           Delete
         </button>
       </div>
